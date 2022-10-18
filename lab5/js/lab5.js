13b5d320f881e628f10b9ab4d522f9f149876b7e
alli5689 <114507183+alli5689@users.noreply.github.com>
2022-10-17 20:21:59 -0700

    Create lab5.js

---
+++ lab5/js/lab5.js
@@ -0,0 +1,21 @@
+/**
+ * Name: Allison Dutton
+ * Created:   10.17.2022
+ * Course: ART-101
+ **/
+
+ // Define Variables
+ make = "Voltzwagon";
+ model = "jetta";
+ year = 2015;
+ color = "grey";
+
+ // calculate
+ age = 2022 - year;
+
+ // output
+document.writeln("Make:"+ make + "<br>");
+document.writeln("Model:"+ model + "<br>");
+document.writeln("Year:"+ year + "<br>");
+document.writeln("Color:"+ color + "<br>");
+document.writeln("Age:"+ age + "years<br>");
