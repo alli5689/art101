4bc2215a4f24ec574c527495b292a1a776b33f32
alli5689 <114507183+alli5689@users.noreply.github.com>
2022-10-17 20:22:26 -0700

    Update lab.js

---
+++ lab4/js/lab.js
@@ -0,0 +1,18 @@
+
+// Summary. (use period)
+// Description. (use period)
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author AuthorName.
+// @since  x.x.x
+
+<pre><code>
+//Computer Tasks: Dress-up game;
+
+ // configure website
+  //	print starter avatar
+   //	create outfit choice images
+    //	drag object animation
+     //	create choice bar
+     //	small change links
+<pre><code>
