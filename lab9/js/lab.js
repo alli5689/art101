553f7d55baa7e7083d66cf2cc3c40dd5b461c35c
alli5689 <114507183+alli5689@users.noreply.github.com>
2022-10-31 15:21:16 -0700

    Create lab.js

---
+++ lab9/js/lab.js
@@ -0,0 +1,13 @@
+  /*
+  * Author: Allison Dutton <adutton@ucsc.edu>
+  * Created: 26 October
+  * Licence: Public Domain
+  */
+
+  var outputEL = document.getElementById("output");
+
+  var new1El = document.createElement("p");
+  new1El.innterhtml = "heyyy shawty ;)";
+
+  var new2El = document.createElement("y");
+  new2El.innerhtml = "xoxo";
