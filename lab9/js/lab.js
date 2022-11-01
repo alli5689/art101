43583cbd5e74adb454bf2f5b272ecd1c950331c2
alli5689 <114507183+alli5689@users.noreply.github.com>
2022-10-31 19:39:04 -0700

    Update lab.js

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -1,13 +1,21 @@
   /*
   * Author: Allison Dutton <adutton@ucsc.edu>
-  * Created: 26 October
+  * Created: 31 October
   * Licence: Public Domain
   */
 
   var outputEL = document.getElementById("output");
 
   var new1El = document.createElement("p");
+  new1El.id = "heyyy shawty ;)";
   new1El.innterhtml = "heyyy shawty ;)";
 
   var new2El = document.createElement("y");
+  new2El.id = "xoxo";
   new2El.innerhtml = "xoxo";
+
+  new1El.appendChild(new2El);
+  document.getElementById("output").appendChild(new1El);
+
+  console.log("hello?", new1El);
+  console.log("Goodbye", new2El);
